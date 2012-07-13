d18906200f17d24bc3d13814918fc25300e7867a
dreachem <dreachem@ead328b1-e903-0410-a19d-eacee4b031f6>
2012-07-13 22:24:41 +0000

    [CAF] Updating copyright info for parts borrowed from OpenSHMEM
    git-svn-id: https://svn.open64.net/svnroot/open64/branches/OpenUH@3980 ead328b1-e903-0410-a19d-eacee4b031f6

--- osprey/libcaf/service.c
+++ osprey/libcaf/service.c
@@ -1,29 +1,43 @@
 /*
- CAF runtime library to be used with OpenUH
+ *
+ * Copyright (c) 2011, 2012
+ *   University of Houston System and Oak Ridge National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * o Neither the name of the University of Houston System, Oak Ridge
+ *   National Laboratory nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific
+ *   prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
- Copyright (C) 2009-2012 University of Houston.
-
- This program is free software; you can redistribute it and/or modify it
- under the terms of version 2 of the GNU General Public License as
- published by the Free Software Foundation.
-
- This program is distributed in the hope that it would be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
- Further, this software is distributed without any warranty that it is
- free of the rightful claim of any third person regarding infringement
- or the like.  Any license provided herein, whether implied or
- otherwise, applies only to this software file.  Patent licenses, if
- any, provided herein do not apply to combinations of this program with
- other software, or any other product whatsoever.
-
- You should have received a copy of the GNU General Public License along
- with this program; if not, write the Free Software Foundation, Inc., 59
- Temple Place - Suite 330, Boston MA 02111-1307, USA.
-
- Contact information:
- http://www.cs.uh.edu/~hpctools
+/*
+ This provides a service thread for ensuring progress. This has been adapted
+ from the UH OpenSHMEM implementation.
 */
 
 /* #defines are in the header file */
@@ -31,6 +45,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <error.h>
+#include <string.h>
 
 /*
  * for hi-res timer
@@ -52,6 +67,8 @@ static struct timespec delayspec;
 static pthread_t thr;
 
 static volatile int done = 0;
+
+extern void comm_service();
 
 /*
  * does comms. service until told not to
