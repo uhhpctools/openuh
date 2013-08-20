77acbbd7d8dccb340188919f33fe77d480ac3b98
dreachem <dreachem@ead328b1-e903-0410-a19d-eacee4b031f6>
2013-08-20 21:01:30 +0000

    [CAF] adds some more front-end syntax checks for CAF
    This adds a few more syntax/semantic checks for CAF programs:
    
    - Check for save attribute for non-dummy coarrays. A coarray that is not a
      dummy argument and does not have the allocatable attribute should have the
      save attribute. This need not be explicitly specified, however, for
      variables declared in the specication part of the main program or a module.
    
    - Check for explicit interface if dummy arg is a coarray
    
    Also, we disallow use of image control statements if coarrays aren't enabled.
    
    Updating patch level to 3.0.31.
    
    Contributor: Deepak
    
    git-svn-id: https://svn.open64.net/svnroot/open64/branches/OpenUH@4073 ead328b1-e903-0410-a19d-eacee4b031f6

--- osprey/crayf90/fe90/s_driver.c
+++ osprey/crayf90/fe90/s_driver.c
@@ -6102,6 +6102,9 @@ void	global_name_semantics(int	def_ga_idx,
                 ATD_ALLOCATABLE(arg_attr_idx) ||
                 ATD_VALUE_ATTR(arg_attr_idx) ||
                 AT_OPTIONAL(arg_attr_idx) ||
+#ifdef _UH_COARRAYS
+                ATD_PE_ARRAY_IDX(arg_attr_idx) != NULL_IDX ||
+#endif
                 (ATD_ARRAY_IDX(arg_attr_idx) != NULL_IDX &&
                  BD_ARRAY_CLASS(ATD_ARRAY_IDX(arg_attr_idx)) == Assumed_Shape)){
                need_expl_itrfc	= TRUE;
